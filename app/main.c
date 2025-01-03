064462783b87311b05116c07f3d9a432a5051861
Ilia Choly <ilia.choly@gmail.com>
2025-01-03 07:38:12 -0500

    loop

--- app/main.c
+++ app/main.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-  printf("$ ");
+  while (1) {
+    // Uncomment this block to pass the first stage
+    printf("$ ");
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
 
-  // remove trailing newline
-  char *nl_index = strstr(input, "\n");
-  if (nl_index != NULL) {
-    *nl_index = 0;
+    // remove trailing newline
+    char *nl_index = strstr(input, "\n");
+    if (nl_index != NULL) {
+      *nl_index = 0;
+    }
+
+    // print error
+    printf("%s: command not found\n", input);
   }
-
-  // print error
-  printf("%s: command not found\n", input);
 
   return 0;
 }
