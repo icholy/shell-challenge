0f58c07f9cbaf6ee3d823e37b802030f932ae1ce
Ilia Choly <ilia.choly@gmail.com>
2025-01-03 07:27:47 -0500

    implement command not found

--- app/main.c
+++ app/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
   // Flush after every printf
@@ -6,6 +7,15 @@ int main() {
 
   // Uncomment this block to pass the first stage
   printf("$ ");
+
+  // read line from stdin
+  char *line = NULL;
+  size_t line_alloc_size;
+  size_t line_size = getline(&line, &line_alloc_size, stdin);
+  if (line_size != -1) {
+    line[line_size-1] = 0; // remove the trailing newline
+    printf("%s: command not found\n", line);
+  }
 
   // Wait for user input
   char input[100];
