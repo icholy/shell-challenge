d7a4871947df3358d7b4ba190e6e748d6a5a92f7
Ilia Choly <ilia.choly@gmail.com>
2025-01-07 07:34:07 -0500

    add check test

---
+++ Makefile
@@ -0,0 +1,11 @@
+CC = gcc
+CFLAGS = -Wall -Wextra -I./app
+LDFLAGS = -lcheck -lm -lsubunit
+SRC = app/command.c app/env_path.c
+
+check:
+	$(CC) tests/*.c $(SRC) $(CFLAGS) $(LDFLAGS) -o test_runner
+	./test_runner
+	rm -f test_runner
+
+.PHONY: check
---
+++ tests/check_env_path.c
@@ -0,0 +1,45 @@
+#include "../app/env_path.h"
+#include <check.h>
+#include <stdlib.h>
+
+START_TEST (env_path_parse_test)
+{
+	struct EnvPath env_path;
+	env_path_parse(&env_path, "/bin:/usr/bin");
+	ck_assert_uint_eq(env_path.n_paths, 2);
+	ck_assert_str_eq(env_path.paths[0], "/bin");
+	ck_assert_str_eq(env_path.paths[1], "/usr/bin");
+}
+END_TEST
+
+Suite * env_path_suite(void)
+{
+	Suite *s;
+	TCase *tc_env_path;
+
+	s = suite_create("EnvPath");
+
+	/* Core test case */
+	tc_env_path = tcase_create("Core");
+
+	tcase_add_test(tc_env_path, env_path_parse_test);
+	suite_add_tcase(s, tc_env_path);
+
+	return s;
+}
+
+int main(void)
+{
+	int number_failed;
+	Suite *s;
+	SRunner *sr;
+
+	s = env_path_suite();
+	sr = srunner_create(s);
+
+	srunner_run_all(sr, CK_NORMAL);
+	number_failed = srunner_ntests_failed(sr);
+	srunner_free(sr);
+	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
