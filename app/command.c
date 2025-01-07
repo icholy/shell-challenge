abd835a420c8c299f4b807a7b168a384205c86be
Ilia Choly <ilia.choly@gmail.com>
2025-01-07 08:23:30 -0500

    stop using strtok

--- app/command.c
+++ app/command.c
@@ -2,32 +2,85 @@
 #include <stdio.h>
 #include <string.h>
 
+int command_parser_init(struct CommandParser *parser, char *input) {
+  parser->offset = 0;
+  parser->input = input;
+  parser->arg = NULL;
+  parser->next = input[0];
+  return 0;
+}
+
+int is_digit(char c) { return '0' <= c && c <= '9'; }
+
+int is_alpha(char c) {
+  return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z');
+}
+
+int is_space(char c) {
+  switch (c) {
+  case ' ':
+  case '\t':
+  case '\n':
+    return 1;
+  default:
+    return 0;
+  }
+}
+
+int is_ident(char c) {
+  if (is_space(c)) {
+    return 0;
+  }
+  return is_digit(c) || is_alpha(c);
+}
+
+void command_parser_inc(struct CommandParser *parser) {
+  parser->offset++;
+  parser->next = parser->input[parser->offset];
+}
+
+int command_parser_next(struct CommandParser *parser) {
+  // skip whitespace
+  while (is_space(parser->next)) {
+    command_parser_inc(parser);
+  }
+  // check for EOF
+  if (parser->next == 0) {
+    parser->arg = NULL;
+    return 0;
+  }
+  // remember the start of the next arg
+  parser->arg = parser->input + parser->offset;
+  // go forward until we hit a space or EOF
+  while (!is_space(parser->next) && parser->next != 0) {
+    command_parser_inc(parser);
+  }
+  // add the null terminator
+  parser->input[parser->offset] = 0;
+  return 0;
+}
+
 /**
  * Parse a space separate command line.
  * This function modifies the input string.
  */
 int command_parse(struct Command *command, char *input) {
-  // remove trailing newline
-  char *end = strchr(input, '\n');
-  if (end != NULL) {
-    *end = 0;
+  struct CommandParser parser;
+  if (command_parser_init(&parser, input) != 0) {
+    return 1;
   }
-
-  size_t narg = 0;
-  char *token = strtok(input, " ");
-
-  command->name = token;
-
-  while (token != NULL) {
-    command->argv[narg] = token;
-    narg++;
-    token = strtok(NULL, " ");
-  }
-  command->argv[narg] = NULL;
-
   command->name = input;
-  command->narg = narg;
-
+  command->narg = 0;
+  while (1) {
+    if (command_parser_next(&parser) != 0) {
+      return 1;
+    }
+    command->argv[command->narg] = parser.arg;
+    if (parser.arg == NULL) {
+      break;
+    }
+    command->narg++;
+  }
   return 0;
 }
 
