a750c91ab9b8c9d3fbce236e7b00603a2c91edfb
Ilia Choly <ilia.choly@gmail.com>
2025-01-11 12:07:29 -0500

    split up the tests

--- tests/check.c
+++ tests/check.c
@@ -47,7 +47,7 @@ START_TEST(command_parse_double_quote) {
 }
 END_TEST
 
-START_TEST(command_parse_escaped_chars) {
+START_TEST(command_parse_escaped_chars_unquoted) {
   struct Command command;
   char *input = strdup("echo hello\\ world file\\\"name file\\\'s");
   command_parse(&command, input);
@@ -56,6 +56,18 @@ START_TEST(command_parse_escaped_chars) {
   ck_assert_str_eq(command_arg(&command, 0), "hello world");
   ck_assert_str_eq(command_arg(&command, 1), "file\"name");
   ck_assert_str_eq(command_arg(&command, 2), "file's");
+  free(input);
+}
+END_TEST
+
+START_TEST(command_parse_escaped_chars_quoted) {
+  struct Command command;
+  char *input = strdup("echo \"hello\\\"world\" 'test\\'s\\\"quoted\\\"'");
+  command_parse(&command, input);
+  ck_assert_int_eq(command.narg, 3);
+  ck_assert_str_eq(command.name, "echo");
+  ck_assert_str_eq(command_arg(&command, 0), "hello\"world");
+  ck_assert_str_eq(command_arg(&command, 1), "test's\"quoted\"");
   free(input);
 }
 END_TEST
@@ -71,7 +83,8 @@ Suite *test_suite(void) {
   tcase_add_test(tc_command, command_parse_simple);
   tcase_add_test(tc_command, command_parse_single_quote);
   tcase_add_test(tc_command, command_parse_double_quote);
-  tcase_add_test(tc_command, command_parse_escaped_chars);
+  tcase_add_test(tc_command, command_parse_escaped_chars_unquoted);
+  tcase_add_test(tc_command, command_parse_escaped_chars_quoted);
   suite_add_tcase(s, tc_command);
 
   return s;
